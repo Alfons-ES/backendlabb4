4a8c22258e517057d810f2f797e635ad9481d1b2
Alfons-ES <alfonsersv@gmail.com>
2026-05-07 10:05:27 +0200

    server.js början

---
+++ api/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(process.env.MONGO_URI);
