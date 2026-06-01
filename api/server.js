694a2dbb0f7dae885ef117d9665d1eb9a1f42248
Alfons-ES <alfonsersv@gmail.com>
2026-06-01 08:56:17 +0200

    starten av projektuppgift

--- api/server.js
+++ api/server.js
@@ -73,7 +73,7 @@ app.post('/api/login', async (req, res) => {
 //skyddar routern, om token är giltlig får man ett välkomstmeddelande
 app.get('/api/protected', authMiddleware, (req, res) => {
     res.json({
-        message: "Välkommen " + req.user.username + "!",
+        message: "Välkommen " + req.user.username,
     });
 });
 
